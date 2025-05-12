0a069b32be8bb18b2141a49d05c66be1a587bf07
vitoria.silva <132918600+visilvagt@users.noreply.github.com>
2025-05-12 12:08:43 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXWYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxwyz'; 
+const numeros = '0123456789';
+const simbolos = '!@%*ç?#-_;,.></{}()';
+const numeroSenha = document.querySelector('.parametro-senha__texto')
+
+
+
